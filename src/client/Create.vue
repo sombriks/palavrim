3d63ec4a264329ce1fc03fd437f70b0a3b64e78f
sombriks <sombriks@gmail.com>
2022-02-06 01:16:47 -0300

    vue3 é bonito

--- src/client/Create.vue
+++ src/client/Create.vue
@@ -1,3 +1,15 @@
+<script setup>
+import { ref } from "vue";
+import { useRouter, useRoute } from "vue-router";
+
+const router = useRouter();
+const route = useRoute();
+
+const word = ref("");
+const criar = (_) => router.push(`/play/${word.value}`);
+</script>
 <template>
-create
+  <h1>Dê uma palavra para desafiar os outros</h1>
+  <input type="text" v-model="word" />
+  <button @click="criar">Criar</button>
 </template>
