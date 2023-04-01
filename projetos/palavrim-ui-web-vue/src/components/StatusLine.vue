591fd9d8d8821dfcb880152371f6ad5e61be5d07
sombriks <sombriks@gmail.com>
2023-04-01 16:55:01 -0300

    add share results

--- projetos/palavrim-ui-web-vue/src/components/StatusLine.vue
+++ projetos/palavrim-ui-web-vue/src/components/StatusLine.vue
@@ -1,13 +1,34 @@
 <template>
-<div>{{ props?.err }}</div>
-<div v-if="!props.match.victory && !props.match.finished">
-  <span><i>Tentativa {{props.match.guesses.length + 1}} de {{ match.maxAttempts}}</i></span>
-</div>
-<div v-if="props.match.finished || props.match.victory">
-  <span v-if="props.match.victory"><i>Vitória! Palavra nova só amanhã!</i></span>
-  <span v-else><i>Derrota! A palavra era "{{ props.match.word.toUpperCase() }}"</i></span>
-</div>
+  <br/>
+  <div>{{ props?.err }}</div>
+  <div v-if="!props.match.victory && !props.match.finished">
+    <span><i>Tentativa {{ props.match.guesses.length + 1 }} de {{ match.maxAttempts }}</i></span>
+  </div>
+  <div v-if="props.match.finished || props.match.victory">
+  <span v-if="props.match.victory">
+    <i>Vitória! Palavra nova só amanhã!</i>&nbsp;
+    <a href="#" @click="share('https://mastodon.world/share')">toot</a>&nbsp;
+    <a href="#" @click="share('https://twitter.com/intent/tweet')">tweet</a>&nbsp;
+  </span>
+    <span v-else><i>Derrota! A palavra era "{{ props.match.word.toUpperCase() }}"</i></span>
+    <pre>{{ text }}</pre>
+  </div>
+  <br/>
 </template>
 <script setup>
+import {computed} from "vue"
+import {shareMatch} from "@/palavrim"
+
 const props = defineProps(["match", "err"])
+const text = computed(() => {
+  if (props.match.finished || props.match.victory) {
+    return shareMatch(props.match)
+  }
+  return ""
+})
+const share = (base) => {
+  const msg = `Adivinhei a palavra!
+  ${text.value}`;
+  window.open(encodeURI(`${base}?text=${msg}&url=${window.location.href}&hashtags=wordle,palavrim`))
+}
 </script>
--- projetos/palavrim-ui-web-vue/src/palavrim.js
+++ projetos/palavrim-ui-web-vue/src/palavrim.js
@@ -2,7 +2,7 @@
  * palavrim core adapted from node version.
  *
  * key differences are the heavy dependency on vue3 reactivity and a more
- * state-specifig configuration here.
+ * state-specific configuration here.
  *
  * most heavy-lifting being made in a reactive way, code here focus on provide a
  * valid state for the game.
@@ -11,91 +11,99 @@
 import wordList from '@/assets/words'
 
 export const todayIs = () =>
-    new Date().toISOString().toString().split("T")[0]
+  new Date().toISOString().toString().split("T")[0]
 
 export const loadPalavrim = () => {
-    let palavrim = localStorage.getItem("palavrim")
-    if(palavrim) {
-        try {
-            palavrim = JSON.parse(palavrim)
-        } catch (e) {
-            console.warn("corrupt state", e)
-            palavrim = {matches:{}}
-        }
+  let palavrim = localStorage.getItem("palavrim")
+  if (palavrim) {
+    try {
+      palavrim = JSON.parse(palavrim)
+    } catch (e) {
+      console.warn("corrupt state", e)
+      palavrim = {matches: {}}
     }
-    else palavrim = {matches:{}}
-    return palavrim
+  } else palavrim = {matches: {}}
+  return palavrim
 }
 
 export const newMatch = (createdAt = todayIs(), maxAttempts = 8) => {
 
-    const match = {
-        maxAttempts,
-        word: wordList[parseInt(`${wordList.length * Math.random()}`)],
-        guesses: [],
-        victory: false,
-        finished: false
-    }
+  const match = {
+    maxAttempts,
+    word: wordList[parseInt(`${wordList.length * Math.random()}`)],
+    guesses: [],
+    victory: false,
+    finished: false
+  }
 
-    return match
+  return match
 }
 
 export const getCurrentMatch = () => {
-    let today = todayIs()
-    let palavrim = loadPalavrim()
+  let today = todayIs()
+  let palavrim = loadPalavrim()
 
-    if(palavrim.matches[today]) return palavrim.matches[today]
-    // else
-    palavrim.matches[today] = newMatch(today)
-    localStorage.setItem("palavrim", JSON.stringify(palavrim))
+  if (palavrim.matches[today]) return palavrim.matches[today]
+  // else
+  palavrim.matches[today] = newMatch(today)
+  localStorage.setItem("palavrim", JSON.stringify(palavrim))
 
-    return palavrim.matches[today]
+  return palavrim.matches[today]
 }
 
 export const saveCurrentMatch = (match) => {
-    match.createdAt ??= todayIs()
-    let palavrim = loadPalavrim()
+  match.createdAt ??= todayIs()
+  let palavrim = loadPalavrim()
 
-    palavrim.matches[match.createdAt] = match
+  palavrim.matches[match.createdAt] = match
 
-    localStorage.setItem("palavrim", JSON.stringify(palavrim))
+  localStorage.setItem("palavrim", JSON.stringify(palavrim))
 }
 
 export const newGuess = ({word}, guess) => {
-    guess = guess.trim().toUpperCase()
-    word = word.trim().toUpperCase()
+  guess = guess.trim().toUpperCase()
+  word = word.trim().toUpperCase()
 
-    if (word.length != guess.length)
-        throw new Error("palavra de tamanho errado")
-    if (!wordList.find(w => w.toUpperCase() === guess))
-        throw new Error(`plavra ${guess} não existe na lista de palavras aceitas`)
+  if (word.length != guess.length)
+    throw new Error("palavra de tamanho errado")
+  if (!wordList.find(w => w.toUpperCase() === guess))
+    throw new Error(`plavra ${guess} não existe na lista de palavras aceitas`)
 
-    const status = []
-    const letters = guess.split("")
-    const secret = word.split("")
+  const status = []
+  const letters = guess.split("")
+  const secret = word.split("")
 
-    let i = letters.length
-    while (i-- > 0) {
-        const letter = letters[i]
-        if (letters[i] == secret[i])
-            status.unshift({letter, exactMatch: true})
-        else if (secret.indexOf(letters[i]) > -1)
-            status.unshift({letter, letterPresent: true})
-        else
-            status.unshift({letter, letterNotPresent: true})
-    }
-    return {word, guess, status}
+  let i = letters.length
+  while (i-- > 0) {
+    const letter = letters[i]
+    if (letters[i] == secret[i])
+      status.unshift({letter, exactMatch: true})
+    else if (secret.indexOf(letters[i]) > -1)
+      status.unshift({letter, letterPresent: true})
+    else
+      status.unshift({letter, letterNotPresent: true})
+  }
+  return {word, guess, status}
 }
 
 export const getLetterColor = (status) => {
-    if (status?.exactMatch) return "var(--match)"
-    if (status?.letterPresent) return "var(--present)"
-    if (status?.letterNotPresent) return "var(--notpresent)"
-    return "var(--blank)" // TODO return theme colors
+  if (status?.exactMatch) return "var(--match)"
+  if (status?.letterPresent) return "var(--present)"
+  if (status?.letterNotPresent) return "var(--notpresent)"
+  return "var(--blank)"
 }
 
 export const isVictory = (match) =>
-    match.guesses.filter(g => g.word === g.guess).length > 0
+  match.guesses.filter(g => g.word === g.guess).length > 0
 
 export const isFinished = (match) =>
-    match.guesses.length === match.maxAttempts
+  match.guesses.length === match.maxAttempts
+
+export const shareMatch = (match) => {
+  return match.guesses.map(g => `${g.status.map(s => {
+    if (s?.exactMatch) return "🟩"
+    if (s?.letterPresent) return "🟧";
+    if (s?.letterNotPresent) return "🟥"
+    return "⬜";
+  }).join("")}` + "\n").join("")
+}
